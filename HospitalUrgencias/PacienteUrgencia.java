dacb10ea0e8db41afa97b05d9ded08af816978b8
NavidUdc <nlobatos@unicartagena.edu.co>
2026-05-27 09:37:49 -0500

    agregando clase lista con los metodos requeridos

--- HospitalUrgencias/PacienteUrgencia.java
+++ HospitalUrgencias/PacienteUrgencia.java
@@ -55,12 +55,17 @@ class PacienteUrgencia{
                 '}';
     }
 
+    @Override
     public boolean equals(Object object) {
+        if (this == object) return true;
         if (object == null || getClass() != object.getClass()) return false;
-        if (!super.equals(object)) return false;
         PacienteUrgencia that = (PacienteUrgencia) object;
-        return codigo_triage == that.codigo_triage && java.util.Objects.equals(nombre, that.nombre) && java.util.Objects.equals(identificacion, that.identificacion) && java.util.Objects.equals(contacto, that.contacto);
+        return java.util.Objects.equals(this.codigo_triage, that.codigo_triage);
     }
 
-    
+    @Override
+    public int hashCode() {
+        return java.util.Objects.hash(this.codigo_triage);
+    }
+   
 }
